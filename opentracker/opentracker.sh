2b5374d8591fb82080637173553a6c6e324fdb41
Simone Flavio <smoxy@users.noreply.github.com>
2023-02-20 20:21:31 +0100

    Update opentracker.sh

--- opentracker/opentracker.sh
+++ opentracker/opentracker.sh
@@ -6,6 +6,10 @@ cd /home/WHOAMI/opentracker_w/ && \
 
 kill -s 1 $(pidof opentracker)
 
-### Avvio delegato a crontab
+### Avvio delegato a crontab:
+##@reboot screen -S opentracker -d -m opentracker -f /home/WHOAMI/opentracker_w/opentracker.conf -w /home/WHOAMI/opentracker_w/whitelist.list
+##*/15 * * * * sh /home/WHOAMI/opentracker_w/opentracker.sh
+
+### Alternativa al kill SIGHUP
 ##screen -X -S opentracker quit && \
 ##    screen -S opentracker -d -m opentracker -f /home/WHOAMI/opentracker_w/opentracker.conf -w /home/WHOAMI/opentracker_w/whitelist.list
